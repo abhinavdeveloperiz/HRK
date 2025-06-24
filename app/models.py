ad83c4c7681eed586566770ca115ce5d37e695f2
abhinavmabhi <dexmarketing192018@gmail.com>
2025-06-24 10:00:36 +0530

    push

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Head(models.Model):
     image = models.ImageField(upload_to='head/')
+    left=models.ImageField(upload_to="head/")
+    right=models.ImageField(upload_to="head/")
 
 class student_gallery(models.Model):
     image = models.ImageField(upload_to='student_gallery/')
